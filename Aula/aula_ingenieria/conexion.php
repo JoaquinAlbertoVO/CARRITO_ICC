1656b511089546c4261e102bf86a846dff72b5fd
Joaquin_Villalobos <joaquinvillalobos110@gmail.com>
2026-07-04 10:38:03 -0500

    fix: remover BOM de .htaccess y arreglar conexion aula
    - Se removió el BOM (Byte Order Mark) oculto en el archivo .htaccess que estaba causando un Error 500 en Apache.
    - Se agregó mysqli_report(MYSQLI_REPORT_OFF) en la conexión del Aula para evitar Fatal Errors por excepciones no capturadas en PHP 8.1+.

--- Aula/aula_ingenieria/conexion.php
+++ Aula/aula_ingenieria/conexion.php
@@ -1,12 +1,12 @@
-<?php  
+<?php
+mysqli_report(MYSQLI_REPORT_OFF);
 $host     = 'localhost';
 $user     = 'icccom_icc';
 $password = 'bpFsCGU@d0sx@zO';
 $db       = 'icccom_icc';
-$conection = @mysqli_connect($host,$user,$password,$db);
+$conection = @mysqli_connect($host, $user, $password, $db);
 
 if (!$conection) {
-	echo "Error de Conexion";
-}else{
+	echo "Error de Conexion: " . mysqli_connect_error();
 }
 ?>

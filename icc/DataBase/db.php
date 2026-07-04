1fb5e07cc9e6129d43eae392b5ac6aed7e4f4def
Joaquin_Villalobos <joaquinvillalobos110@gmail.com>
2026-07-04 10:31:04 -0500

    chore: limpieza de carpetas legacy obsoletas
    - Se eliminaron las carpetas 'icc', 'icc_aula', 'aulagregre' y 'qr' del repositorio.
    - Estas contenían versiones antiguas del aula virtual, administradores antiguos y archivos de prueba que no pertenecen a la nueva arquitectura MVC.

--- icc/DataBase/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-  $host = "localhost";
-  $username = "icccom_app";
-  $password = "bpFsCGU@d0sx@zO";
-  $dbname = "icccom_app";
-
-  $conexion = mysqli_connect($host,$username,$password,$dbname);
-  
-?>

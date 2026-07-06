d2d981ee7d0eb86a3bab71bc937506dddc0ebd29
Joaquin_Villalobos <joaquinvillalobos110@gmail.com>
2026-07-06 14:36:02 -0500

    aea

---
+++ check_a11y.php
@@ -0,0 +1,14 @@
+﻿<?php
+$html = file_get_contents('app/Views/layouts/main.php');
+preg_match_all('/<a[^>]*>.*?<\/a>/is', $html, $links);
+foreach ($links[0] as $link) {
+    if (!preg_match('/aria-label/', $link) && !preg_match('/[a-zA-Z0-9]/', strip_tags($link))) {
+        echo "Link without text/aria: " . htmlspecialchars($link) . "\n";
+    }
+}
+preg_match_all('/<button[^>]*>.*?<\/button>/is', $html, $buttons);
+foreach ($buttons[0] as $btn) {
+    if (!preg_match('/aria-label/', $btn) && !preg_match('/[a-zA-Z0-9]/', strip_tags($btn))) {
+        echo "Button without text/aria: " . htmlspecialchars($btn) . "\n";
+    }
+}

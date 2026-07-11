b33fa6069d4c92ccdfaac1a7a0cad15fe0988f12
Rodrigo_Alonso_Dc <porlagloriadefrancisco050121@gmail.com>
2026-07-11 11:16:56 -0500

    fix: eliminar error de sintaxis en editar.php (etiqueta PHP malformada)

--- app/Views/admin/ingenieria/editar.php
+++ app/Views/admin/ingenieria/editar.php
@@ -1,4 +1,3 @@
-<?php if (!empty(\)) echo \; ?>
 <div class="container-fluid page__heading-container">
         <div class="page__heading d-flex align-items-center justify-content-between">
             <h1 class="m-0">Actualizar Curso</h1>

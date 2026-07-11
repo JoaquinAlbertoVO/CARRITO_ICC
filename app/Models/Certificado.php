9f4866c1cf58316dd86eb26667bdef98768ec08a
Rodrigo_Alonso_Dc <porlagloriadefrancisco050121@gmail.com>
2026-07-11 16:34:46 -0500

    fix: imprimir horas academicas junto al numero dinamicamente

--- app/Models/Certificado.php
+++ app/Models/Certificado.php
@@ -53,10 +53,20 @@ class Certificado {
         $blanco = imagecolorallocate($imagen, 255, 255, 255);
         imagettftext($imagen, 16, 0, 80, 1150, $blanco, $font_path, "Emitido: " . $fecha_emision);
 
-        // 6. Horas lectivas barra izquierda (solo el numero centrado encima de 'horas lectivas')
-        $bbox3 = imagettfbbox(60, 0, $font_path, $horas);
-        $x_horas = (int)(140 - ($bbox3[2] / 2)); // Movido a la izquierda
-        imagettftext($imagen, 60, 0, $x_horas, 430, $blanco, $font_path, $horas);
+        // 6. Horas lectivas barra izquierda (número grande, texto pequeño al lado)
+        $horas_text = $horas;
+        $bbox_h = imagettfbbox(50, 0, $font_path, $horas_text);
+        $w_h = $bbox_h[2] - $bbox_h[0];
+        
+        $acad_text = " horas académicas";
+        $bbox_a = imagettfbbox(24, 0, $font_path, $acad_text);
+        $w_a = $bbox_a[2] - $bbox_a[0];
+        
+        $total_w = $w_h + $w_a;
+        $start_x = (int)(200 - ($total_w / 2)); // Centrado aprox en la barra lateral
+        
+        imagettftext($imagen, 50, 0, $start_x, 410, $blanco, $font_path, $horas_text);
+        imagettftext($imagen, 24, 0, $start_x + $w_h, 410, $blanco, $font_path, $acad_text);
 
         return $imagen;
     }

dbc06ed4970cacb2d835df2414227eb5db322784
Rodrigo_Alonso_Dc <porlagloriadefrancisco050121@gmail.com>
2026-07-13 14:35:40 -0500

    fix: actualizar client-id de paypal a produccion

--- app/Views/checkout/index.php
+++ app/Views/checkout/index.php
@@ -16,8 +16,8 @@
     <!-- Alpine.js -->
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 
-    <!-- PayPal SDK (Reemplazado con tu Client ID de producción) -->
-    <script src="https://www.paypal.com/sdk/js?client-id=EP2fhOGemUEgzmICxDQ4tkk42-5RWckWMvoP8lwEW3Y3EFkXSLv53ncMv38xgubX3yCXGOrgAiUNRe6X&currency=USD"></script>
+    <!-- PayPal SDK (client-id=sb es para pruebas/sandbox, cámbialo por tu Client ID de producción) -->
+    <script src="https://www.paypal.com/sdk/js?client-id=BAAqiauJCgNIFSWMjIrbxzcIlAn6mEzi0uhKYnoN48a_57G7zfy8kInsweY2544eHBiTuc8YQRZKsckGUw&currency=USD"></script>
 </head>
 
 <body>

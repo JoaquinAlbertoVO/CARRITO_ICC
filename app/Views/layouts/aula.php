06f5093ae68a4404df74ebe8f619a4d224463b69
Joaquin_Villalobos <joaquinvillalobos110@gmail.com>
2026-08-10 11:51:58 -0500

    Agregar Meta Pixel a las vistas del Aula Virtual

--- app/Views/layouts/aula.php
+++ app/Views/layouts/aula.php
@@ -19,6 +19,24 @@
     <link type="text/css" href="<?= BASE_URL ?>assets/aula/css/vendor-fontawesome-free.css" rel="stylesheet">
     <link type="text/css" href="<?= BASE_URL ?>assets/aula/css/vendor-ion-rangeslider.css" rel="stylesheet">
     <link type="text/css" href="<?= BASE_URL ?>assets/css/modern_override.css" rel="stylesheet">
+    
+    <!-- Meta Pixel Code -->
+    <script>
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '2480034172420246');
+    fbq('track', 'PageView');
+    </script>
+    <noscript><img height="1" width="1" style="display:none"
+    src="https://www.facebook.com/tr?id=2480034172420246&ev=PageView&noscript=1"
+    /></noscript>
+    <!-- End Meta Pixel Code -->
 </head>
 <body class="layout-fixed layout-sticky-subnav">
 
